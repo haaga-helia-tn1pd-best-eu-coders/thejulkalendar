e7f1160c428e7c1997c197567a475ee84fee8dd7
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2017-11-28 01:37:01 +0200

    Add settings.js file, fix opening lids of previous days

---
+++ scripts/settings.js
@@ -0,0 +1,3 @@
+const calendarActivationMonth = 11     //  Change this to the month you wish to use the calendar in
+const calendarActivationYear = 2017
+const lightRopeBulpAmount = 85 // How many lightbulps the "lightrope" on the top of the screen should have
